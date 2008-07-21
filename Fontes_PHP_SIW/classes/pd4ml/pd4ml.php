38e59028e89176e6fb031b77a40b8b5b7996ad40
alexvp <alexvp@localhost>
2008-07-21 12:36:27 +0000

    Implementa chamada java para geração do PDF tratando o sistema operacional do servidor.

--- Fontes_PHP_SIW/classes/pd4ml/pd4ml.php
+++ Fontes_PHP_SIW/classes/pd4ml/pd4ml.php
@@ -1,28 +1,30 @@
 <?php
 if ($_SERVER["HTTPS"] != "on") {
-	// MS IE needs to cache PDF obtained by HTTPS.
-	header('Pragma: no-cache');
-	header('Expires: -10000');
+  // MS IE needs to cache PDF obtained by HTTPS.
+  header('Pragma: no-cache');
+  header('Expires: -10000');
 }
 
 if (array_key_exists('url', $_POST)) {
 
-	$w_orientation = strtoupper($_POST['orientation']);
-	$w_width = (($w_orientation=='PORTRAIT') ? 800 : 1128);
+  $w_orientation = strtoupper($_POST['orientation']);
+  $w_width = (($w_orientation=='PORTRAIT') ? 800 : 1128);
 
   header('Content-type: application/pdf');
-	header('Content-disposition: inline');
-	//header('Content-disposition: attachment; filename=arquivo.pdf');
+  header('Content-disposition: inline');
+  //header('Content-disposition: attachment; filename=arquivo.pdf');
 
-	// UNIX version
-	//passthru('java -Xmx512m -Djava.awt.headless=true -cp .:pd4ml_demo.jar Pd4Php \'' . $_POST['url'] . '\' 800 A4 2>&1');
-	passthru('java -Xmx512m -Djava.awt.headless=true -cp .:pd4ml.jar Pd4Php \'' . $_POST['url'] . '\' '.$w_width.' A4 \'' . $_POST['orientation'] . '\' 2>&1');
+  if (strpos(strtoupper(PHP_OS),'WIN')===false) {
+    // UNIX version
+    passthru('java -Xmx512m -Djava.awt.headless=true -cp .:pd4ml.jar Pd4Php \'' . $_POST['url'] . '\' '.$w_width.' A4 \'' . $_POST['orientation'] . '\' 2>&1');
+  } else {
     // Windows version
-	//passthru('java -Xmx512m -cp .;pd4ml_demo.jar Pd4Php ' . $_POST['url'] . ' 800 A4');
-    unlink($_POST['filename']);  
+    passthru('java -Xmx512m -cp .;pd4ml.jar Pd4Php ' . $_POST['url'] . ' '.$w_width.' A4 \'' . $_POST['orientation'] . '\'');
+  }
+  unlink($_POST['filename']);  
     
 
 } else {
-	echo 'Uso Inválido  ';
+  echo 'Uso Inválido  ';
 }
 ?>
