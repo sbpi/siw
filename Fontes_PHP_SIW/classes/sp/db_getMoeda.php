e5ee8f67b9a7ae6df29f99483b8ff5a2e61c1029
leandro <leandro@localhost>
2008-08-05 21:30:26 +0000

    Menus de Moedas foi adicionado as tabelas de Instituições Financeira em Controle

---
+++ Fontes_PHP_SIW/classes/sp/db_getMoeda.php
@@ -0,0 +1,33 @@
+<?
+extract($GLOBALS); include_once($w_dir_volta.'classes/db/DatabaseQueriesFactory.php');
+/**
+* class sp_getMoeda
+*
+* { Description :- 
+*    Recupera as unidades monetárias existentes.
+* }
+*/
+
+class db_getMoeda {
+   function getInstanceOf($dbms, $p_chave, $p_restricao, $p_nome, $p_ativo, $p_sigla) {
+     extract($GLOBALS,EXTR_PREFIX_SAME,'strchema'); $sql=$strschema.'sp_getMoeda';
+     $params=array('p_chave'    =>array($p_chave,       B_INTEGER,     32),
+                   'p_restricao'=>array($p_restricao,   B_VARCHAR,     30),
+                   'p_nome'     =>array($p_nome,        B_VARCHAR,     60),
+                   'p_ativo'    =>array($p_ativo,       B_VARCHAR,      1),
+                   'p_sigla'    =>array($p_sigla,       B_VARCHAR,      3),
+                   'p_result'   =>array(null,           B_CURSOR,      -1)
+                  );
+     $l_rs = DatabaseQueriesFactory::getInstanceOf($sql, $dbms, $params, DB_TYPE);
+     $l_error_reporting = error_reporting(); error_reporting(0); if(!$l_rs->executeQuery()) { error_reporting($l_error_reporting); TrataErro($sql, $l_rs->getError(), $params, __FILE__, __LINE__, __CLASS__); }
+     else {
+       error_reporting($l_error_reporting); 
+        if ($l_rs = $l_rs->getResultData()) {
+          return $l_rs;
+        } else {
+          return array();
+        }
+     }
+   }
+}
+?>
---
+++ Fontes_PHP_SIW/classes/sp/dml_CoMoeda.php
@@ -0,0 +1,38 @@
+<?
+extract($GLOBALS); include_once($w_dir_volta.'classes/db/DatabaseQueriesFactory.php');
+/**
+* class dml_CoMoeda
+*
+* { Description :- 
+*    Manipula registros de CO_MOEDA
+* }
+*/
+
+class dml_CoMoeda {
+   function getInstanceOf($dbms, $operacao, $chave, $p_codigo, $p_nome, $p_sigla, $p_simbolo, 
+           $p_tipo, $p_exclusao_ptax, $p_ativo) {
+     extract($GLOBALS,EXTR_PREFIX_SAME,'strchema'); 
+     $sql=$strschema.'sp_putCoMoeda';
+     $params=array('operacao'          =>array($operacao,          B_VARCHAR,      1),
+                   'chave'             =>array($chave,             B_NUMERIC,     32),
+                   'p_codigo'          =>array($p_codigo,          B_VARCHAR,      3),
+                   'p_nome'            =>array($p_nome,            B_VARCHAR,     30),
+                   'p_sigla'           =>array($p_sigla,           B_VARCHAR,      5),
+                   'p_simbolo'         =>array($p_simbolo,         B_VARCHAR,     10),
+                   'p_tipo'            =>array($p_tipo,            B_VARCHAR,      1),
+                   'p_exclusao_ptax'    =>array($p_exclusao_ptax,  B_DATE,        32),
+                   'p_ativo'           =>array($p_ativo,           B_VARCHAR,      1)
+                  );
+     $l_rs = DatabaseQueriesFactory::getInstanceOf($sql, $dbms, $params, DB_TYPE);
+     $l_error_reporting = error_reporting(); 
+     error_reporting(0); 
+     if(!$l_rs->executeQuery()) { 
+       error_reporting($l_error_reporting); 
+       TrataErro($sql, $l_rs->getError(), $params, __FILE__, __LINE__, __CLASS__); 
+     } else {
+       error_reporting($l_error_reporting); 
+       return true;
+     }
+   }
+}
+?>
