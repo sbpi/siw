cffc0d4fe24842947f9e2bfa3fcba118f8b8df5b
alexvp <alexvp@localhost>
2008-09-17 18:16:19 +0000

    Ajuste na função que indica o último dia do mês.

--- SP_MSSQL2000/SIW/LastDay.sql
+++ SP_MSSQL2000/SIW/LastDay.sql
@@ -1,25 +1,7 @@
-SET QUOTED_IDENTIFIER ON 
-GO
-SET ANSI_NULLS ON 
-GO
-
-alter  FUNCTION [dbo].[LastDay] ( @pInputDate    DATETIME )
+alter FUNCTION dbo.LastDay ( @pInputDate    DATETIME )
 RETURNS DATETIME
 BEGIN
 
-    DECLARE @vOutputDate        DATETIME
-
-    SET @vOutputDate = CAST(YEAR(@pInputDate) AS VARCHAR(4)) + '/' + 
-                       CAST(MONTH(@pInputDate) AS VARCHAR(2)) + '/01'
-    SET @vOutputDate = DATEADD(DD, -1, DATEADD(M, 1, @vOutputDate))
-
-    RETURN @vOutputDate
+    RETURN dateadd(day,-1* day(dateadd(month,1,@pInputDate)),dateadd(month,1,@pInputDate))
 
 END
-
-GO
-SET QUOTED_IDENTIFIER OFF 
-GO
-SET ANSI_NULLS ON 
-GO
-
