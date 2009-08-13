4286b61d05e9c5452f414bbd5a1bd5602f46caac
alexvp <alexvp@localhost>
2009-08-13 15:55:14 +0000

    Views para integração.

--- SP_Oracle92/siw/VW_ACORDO.sql
+++ SP_Oracle92/siw/VW_ACORDO.sql
@@ -1,19 +1,11 @@
-CREATE OR REPLACE VIEW VW_ACORDO AS
-  select a.sq_menu, a1.codigo_interno as cd_projeto, a.codigo_interno cd_acordo,
-         b.inicio, b.fim, 
-         c.ordem, c.vencimento, c.valor, c.quitacao
-    from siw_solicitacao                          a
-         inner            join siw_solicitacao    a1 on (a.sq_solic_pai       = a1.sq_siw_solicitacao)
-         inner            join ac_acordo          b  on (a.sq_siw_solicitacao = b.sq_siw_solicitacao)
-           inner          join co_pessoa          g  on (b.outra_parte        = g.sq_pessoa)
-           inner          join siw_tramite        j  on (a.sq_siw_tramite     = j.sq_siw_tramite)
-           inner          join ac_acordo_parcela  c  on (b.sq_siw_solicitacao = c.sq_siw_solicitacao)
-             left outer   join (select x.sq_acordo_parcela, count(*) as existe
-                                  from fn_lancamento                x
-                                       inner   join siw_solicitacao y on (x.sq_siw_solicitacao = y.sq_siw_solicitacao)
-                                         inner join siw_tramite     z on (y.sq_siw_tramite     = z.sq_siw_tramite and
-                                                                          'CA'                 <> Nvl(z.sigla,'CA')
-                                                                         )
-                                group by x.sq_acordo_parcela
-                               )                  d  on (c.sq_acordo_parcela  = d.sq_acordo_parcela)
-   where (b.financeiro_unico = 'N' or (b.financeiro_unico = 'S' and coalesce(d.existe,0) = 0));
+create or replace view vw_acordo as
+select z.sq_pessoa as cliente, z.nome as nm_menu, z.sq_menu,
+         a1.codigo_interno as cd_projeto, a.codigo_interno cd_acordo,
+         b.inicio, b.fim
+    from siw_menu                            z
+         inner       join siw_solicitacao    a  on (z.sq_menu            = a.sq_menu)
+           inner     join siw_solicitacao    a1 on (a.sq_solic_pai       = a1.sq_siw_solicitacao)
+           inner     join ac_acordo          b  on (a.sq_siw_solicitacao = b.sq_siw_solicitacao)
+             inner   join co_pessoa          g  on (b.outra_parte        = g.sq_pessoa)
+             inner   join siw_tramite        j  on (a.sq_siw_tramite     = j.sq_siw_tramite)
+
