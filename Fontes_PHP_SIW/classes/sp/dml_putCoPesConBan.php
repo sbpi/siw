de5e8c1f99d41eb80e25cc5fb1eecc1317424543
cesar <cesar@localhost>
2009-08-20 15:16:46 +0000

    Criação de novo campo no cadastro de contas bancárias.

--- Fontes_PHP_SIW/classes/sp/dml_putCoPesConBan.php
+++ Fontes_PHP_SIW/classes/sp/dml_putCoPesConBan.php
@@ -1,4 +1,4 @@
-<?
+<?php
 extract($GLOBALS); include_once($w_dir_volta.'classes/db/DatabaseQueriesFactory.php');
 /**
 * class dml_PutCoPesConBan
@@ -9,17 +9,18 @@ extract($GLOBALS); include_once($w_dir_volta.'classes/db/DatabaseQueriesFactory.
 */
 
 class dml_PutCoPesConBan {
-   function getInstanceOf($dbms, $p_operacao, $p_chave, $p_pessoa, $p_tipo_conta, $p_agencia, $p_oper, $p_numero, $p_ativo, $p_padrao) {
+   function getInstanceOf($dbms, $p_operacao, $p_chave, $p_pessoa, $p_tipo_conta, $p_agencia, $p_oper, $p_numero, $p_devolucao, $p_ativo, $p_padrao) {
      extract($GLOBALS,EXTR_PREFIX_SAME,'strchema'); $sql=$strschema.'sp_putCoPesConBan';
-     $params=array('p_operacao'           =>array($p_operacao,          B_VARCHAR,      1),
-                   'p_chave'              =>array($p_chave,             B_NUMERIC,     32),
-                   'p_pessoa'           =>array($p_pessoa,          B_NUMERIC,     32),
-                   'p_agencia'          =>array($p_agencia,         B_NUMERIC,     32),
-                   'p_oper'             =>array($p_oper,            B_VARCHAR,      6),
-                   'p_numero'           =>array($p_numero,          B_VARCHAR,     30),
-                   'p_tipo_conta'       =>array($p_tipo_conta,      B_VARCHAR,      1),
-                   'p_ativo'            =>array($p_ativo,           B_VARCHAR,      4),
-                   'p_padrao'           =>array($p_padrao,          B_VARCHAR,      1)
+     $params=array('p_operacao'         =>array($p_operacao,             B_VARCHAR,      1),
+                   'p_chave'            =>array(tvl($p_chave),           B_NUMERIC,     32),
+                   'p_pessoa'           =>array(tvl($p_pessoa),          B_NUMERIC,     32),
+                   'p_agencia'          =>array(tvl($p_agencia),         B_NUMERIC,     32),
+                   'p_oper'             =>array(tvl($p_oper),            B_VARCHAR,      6),
+                   'p_numero'           =>array(tvl($p_numero),          B_VARCHAR,     30),
+                   'p_tipo_conta'       =>array(tvl($p_tipo_conta),      B_NUMERIC,     32),
+                   'p_devolucao'        =>array(tvl($p_devolucao),       B_VARCHAR,      1),                   
+                   'p_ativo'            =>array(tvl($p_ativo),           B_VARCHAR,      1),
+                   'p_padrao'           =>array(tvl($p_padrao),          B_VARCHAR,      1)
                   );
      $l_rs = DatabaseQueriesFactory::getInstanceOf($sql, $dbms, $params, DB_TYPE);
      $l_error_reporting = error_reporting(); 
