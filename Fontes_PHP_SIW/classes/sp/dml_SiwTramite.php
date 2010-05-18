a7768ad23fada6517aabd18deb92c76e01ff052b
cesar <cesar@localhost>
2010-05-18 18:24:51 +0000

    Criação de nova opção na criação de trâmites: Gestor do módulo pode cumprir trâmite?

--- Fontes_PHP_SIW/classes/sp/dml_SiwTramite.php
+++ Fontes_PHP_SIW/classes/sp/dml_SiwTramite.php
@@ -11,7 +11,7 @@ extract($GLOBALS); include_once($w_dir_volta.'classes/db/DatabaseQueriesFactory.
 class dml_SiwTramite {
    function getInstanceOf($dbms, $operacao, $chave, $p_sq_menu, $p_nome, $p_ordem, $p_sigla, $p_descricao,
                    $p_chefia_imediata, $p_ativo, $p_solicita_cc, $p_envia_mail, $p_destinatario, $p_anterior,
-                   $p_beneficiario) {
+                   $p_beneficiario,$p_gestor) {
      extract($GLOBALS,EXTR_PREFIX_SAME,'strchema'); $sql=$strschema.'sp_putSiwTramite';
      $params=array('operacao'          =>array($operacao,          B_VARCHAR,      1),
                    'chave'             =>array($chave,             B_NUMERIC,     32),
@@ -26,7 +26,8 @@ class dml_SiwTramite {
                    'p_envia_mail'      =>array($p_envia_mail,      B_VARCHAR,      1),
                    'p_destinatario'    =>array($p_destinatario,    B_VARCHAR,      1),
                    'p_anterior'        =>array($p_anterior,        B_VARCHAR,      1),
-                   'p_beneficiario'    =>array($p_beneficiario,    B_VARCHAR,      1)
+                   'p_beneficiario'    =>array($p_beneficiario,    B_VARCHAR,      1),
+                   'p_gestor'          =>array($p_gestor,          B_VARCHAR,      1)
                   );
      $l_rs = DatabaseQueriesFactory::getInstanceOf($sql, $dbms, $params, DB_TYPE);
      $l_error_reporting = error_reporting(); 
