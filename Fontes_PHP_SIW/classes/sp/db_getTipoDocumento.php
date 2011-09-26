735bd4f2ea26180851c913e499a438d3d78efe6a
alexvp <alexvp@localhost>
2011-09-26 16:33:18 +0000

    Ajustes no módulo de material.

--- Fontes_PHP_SIW/classes/sp/db_getTipoDocumento.php
+++ Fontes_PHP_SIW/classes/sp/db_getTipoDocumento.php
@@ -10,11 +10,12 @@ include_once($w_dir_volta.'classes/db/DatabaseQueriesFactory.php');
 */
 
 class db_getTipoDocumento {
-   function getInstanceOf($dbms, $p_chave, $p_cliente, $p_menu) {
+   function getInstanceOf($dbms, $p_chave, $p_cliente, $p_menu, $p_restricao) {
      extract($GLOBALS,EXTR_PREFIX_SAME,'strchema'); $sql=$strschema.'sp_getTipoDocumento';
      $params=array('p_chave'                     =>array(tvl($p_chave),                                    B_INTEGER,        32),
                    'p_cliente'                   =>array($p_cliente,                                       B_INTEGER,        32),
                    'p_menu'                      =>array(tvl($p_menu),                                     B_INTEGER,        32),
+                   'p_restricao'                 =>array($p_restricao,                                     B_VARCHAR,        20),
                    'p_result'                    =>array(null,                                             B_CURSOR,         -1)
                   );
      $lql = new DatabaseQueriesFactory; $l_rs = $lql->getInstanceOf($sql, $dbms, $params, DB_TYPE);
